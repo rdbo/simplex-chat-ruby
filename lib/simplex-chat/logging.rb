37299af0d541c36f660688b2e0bf659ff16642c8
rdbo <rdbodev@gmail.com>
2025-03-14 09:59:56 +0000

    reuse same logger instance for everyone

--- lib/simplex-chat/logging.rb
+++ lib/simplex-chat/logging.rb
@@ -2,17 +2,19 @@ require 'logger'
 
 module SimpleXChat
   module Logging
+    @@logger = nil
+
     def self.logger(dest: $stderr, log_level: Logger::INFO)
-      if @logger == nil
-        @logger = Logger.new(dest)
-        @logger.level = log_level
-        @logger.progname = 'simplex-chat'
-        @logger.formatter = -> (severity, datetime, progname, msg) {
+      if @@logger == nil
+        @@logger = Logger.new(dest)
+        @@logger.level = log_level
+        @@logger.progname = 'simplex-chat'
+        @@logger.formatter = -> (severity, datetime, progname, msg) {
           "| [#{severity}] | #{datetime} | (#{progname}) :: #{msg}\n"
         }
       end
 
-      @logger
+      @@logger
     end
   end
 end
